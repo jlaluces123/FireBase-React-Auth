ae676443f28f52d7557baaeb88cdd62274e2c5a9
jlaluces123 <jlalucescareer123@gmail.com>
2019-02-09 17:57:47 -0800

    SignOut: Created a signout button in Home.js -> works as intended

--- firebase-react-auth/src/views/Home.js
+++ firebase-react-auth/src/views/Home.js
@@ -3,11 +3,15 @@ import fire from '../config/fire';
 
 class Home extends React.Component {
 
+  handleLogout = () => {
+    fire.auth().signOut();
+  }
 
   render() {
     return (
       <div className = "home-wrapper">
         <h3>Home Page</h3>
+        <button onClick = {this.handleLogout}>Logout</button>
       </div>
     );
   }
